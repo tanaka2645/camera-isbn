c44e07e7e2e65e862c7dde7b522e4e3b870a6f20
tanakaH <75516520+tanaka2645@users.noreply.github.com>
2022-02-27 16:07:50 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./assets/App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
